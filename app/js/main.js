f256159ba2ba0959875bf59feb05755d420da1c4
Oleksii <oleksiikuz@ukr.net>
2021-02-22 23:06:24 +0200

    initial commit

---
+++ app/js/main.js
@@ -0,0 +1,40 @@
+//Slide
+new Swiper('.swiper-container', {
+	navigation: {
+		nextEl: '.arrow__right',
+		prevEl: '.arrow__left',
+
+	},
+
+	pagination: {
+		el: '.benefits__slider-counter',
+		type: 'fraction'
+	},
+
+	slidesPerView: 1,
+	loop: true,
+})
+
+//Fixed header
+const scrollMenu = function (e) {
+	let element = document.querySelector(".header");
+	let scrollTop = window.scrollY;
+
+	if (scrollTop > 100) {
+		element.classList.add("active");
+	} else {
+		element.classList.remove("active");
+	}
+};
+
+document.addEventListener("scroll", scrollMenu);
+
+
+//Burger menu
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', function () {
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
